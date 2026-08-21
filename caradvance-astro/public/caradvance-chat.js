4d3a6c3609e046725dc24316b11f89f0c3ee1a5e
Caradvance <info@caradvance.hu>
2026-08-22 01:59:28 +0200

    Add files via upload

--- caradvance-astro/public/caradvance-chat.js
+++ caradvance-astro/public/caradvance-chat.js
@@ -265,7 +265,7 @@
       [],
       { "Előnyök": "/vasarlas-elonyei", "Finanszírozás – lízing": "/finanszirozas-lizing" },
       [{ t: "Gyakori kérdések", href: "/gyakori-kerdesek" }]);
-    syncNavMenu(wrap, "Rólunk", [], { "Media": "/media" }, []);
+    syncNavMenu(wrap, "Rólunk", [], { "Media": "/media", "Blog": "/blog" }, []);
   }
 
   ready(run);
